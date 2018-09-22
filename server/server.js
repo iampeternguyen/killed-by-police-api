066a59739e72bad5934430c23259621d5a5f9be0
iampeternguyen <ptnguyen1987@gmail.com>
2018-09-22 16:40:47 +0700

    preparing for push to heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,24 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.get('/', (req, res) => {
+	res.send(
+		`API to get data from killedbypolice.net. <br>
+		This project scraped data from that site to make it easily retrievable for other uses. <br>
+		To use send /GET request to /data for all data or /data/year to retreive data for a specific year.<br>
+		Data exists from 2013-2018.<br>
+		Please note that request all data from /data may take a long time to load.`
+	);
+});
+
+app.get('/data/', (req, res) => {
+	let year = req.params.year;
+	Person.find()
+		.then(people => {
+			res.send(people);
+		})
+		.catch(err => res.status(404).send(`Cannot find data for year ${year}`));
+});
 
 app.get('/data/:year', (req, res) => {
 	let year = req.params.year;
